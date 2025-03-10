bb9d25433b00e270bcf4c2eefd1b665006b7a6fa
axel <i@amusaev.ru>
2025-03-11 00:19:49 +0300

    add InvalidMessageFormatException

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,17 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            sendMessage("Helo");
+        } catch (InvalidMessageFormatException e) {
+            System.out.println("Catch InvalidMessageFormatException: " + e.getMessage());
+        }
+    }
+
+    public static void sendMessage(String message) throws InvalidMessageFormatException {
+        if (message.length() < 5) {
+            throw new InvalidMessageFormatException("The message contains less than 5 characters");
+        }
+
+        System.out.println(message);
     }
 }
